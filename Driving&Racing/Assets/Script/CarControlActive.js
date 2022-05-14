7b8233815ebce8ec8acc264ec6bd09c9bf4d5f77
unknown <lvkhang260900@gmail.com>
2022-05-14 23:15:00 +0700

    Add Driving&Racing file

--- Driving&Racing/Assets/Script/CarControlActive.js
+++
@@ -1,7 +0,0 @@
-﻿var CarControl : GameObject;
-var DreamCar01 : GameObject;
-
-function Start () {
-	CarControl.GetComponent("CarController").enabled = true;
-    DreamCar01.GetComponent("CarAIControl").enabled = true;
-}
